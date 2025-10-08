cd985c4547bbb89fe4d6f60d6c26b29adebb66a8
Ketsy <ketsy.prophete@gmail.com>
2025-10-08 13:09:33 -0400

    changed shape of model

--- ChurchTiming.Api/Data/Run.cs
+++ ChurchTiming.Api/Data/Run.cs
@@ -10,6 +10,8 @@ public class Run
     public int PreteachSec { get; set; }
     public int WalkBufferSec { get; set; }
     public int BaseOfferingSec { get; set; }
+    public int? EnglishOfferingStartedAtSec { get; set; }
+
     public int? SpanishSermonEndedAtSec { get; set; }
     public int? SpanishSermonEndEtaSec { get; set; }
     public List<RundownSegment> Segments { get; set; } = new();
