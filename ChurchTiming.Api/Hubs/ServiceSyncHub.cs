94abae32bb61cc732a0a23bdc1da259c6cf4d99b
Ketsy <ketsy.prophete@gmail.com>
2025-10-08 13:11:08 -0400

    added joinRun and leavRun

--- ChurchTiming.Api/Hubs/ServiceSyncHub.cs
+++ ChurchTiming.Api/Hubs/ServiceSyncHub.cs
@@ -2,4 +2,11 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace ChurchTiming.Api.Contracts;
 
-public class ServiceSyncHub : Hub<ISyncClient> { }
+public class ServiceSyncHub : Hub<ISyncClient>
+{
+    public Task JoinRun(Guid runId) =>
+        Groups.AddToGroupAsync(Context.ConnectionId, runId.ToString());
+
+    public Task LeaveRun(Guid runId) =>
+        Groups.RemoveFromGroupAsync(Context.ConnectionId, runId.ToString());
+}
